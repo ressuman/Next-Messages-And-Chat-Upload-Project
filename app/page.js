ed68375a9e6e320ba4ad732bbe39814f21ed53fd
Richard Essuman <ressuman001@gmail.com>
2024-08-28 14:38:55 +0000

    "Added global CSS styles and updated layout and page components"

--- app/page.js
+++ app/page.js
@@ -1,5 +1,16 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <>
+      <h1>Welcome to Messaging & Chat Upload</h1>
+      <p>
+        Stay connected with your friends and colleagues through our advanced
+        messaging features. Effortlessly share files and media with seamless
+        chat uploads.
+      </p>
+      <p>
+        Next.js performs some pretty aggressive caching. Understanding how it
+        works is key to avoiding issues and ensuring smooth communication.
+      </p>
+    </>
+  );
 }
